9b3c51e961c22dbec0bfd30f60e399d773203587
Liam Simmons <liam.simmons@powerhrg.com>
2024-05-08 08:57:36 -0400

    Add cat functions

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat() {
+  cats.push('Ralph')
+} 
+
+function destructivelyPrependCat() {
+  cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift()
+}
+
+function appendCat(name) {
+  const newCatList = [...cats, name]
+  return newCatList
+}
+
+function prependCat(name) {
+  const anotherNewCatList = [name, ...cats]
+  return anotherNewCatList
+}
+
+function removeLastCat() {
+  const thirdNewCatList = cats.slice (0, cats.length - 1)
+  return thirdNewCatList
+}
+
+function removeFirstCat() {
+  const fourthNewCatList = cats.slice (1, cats.length)
+  return fourthNewCatList
+}
